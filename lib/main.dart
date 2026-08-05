c3f57e2daa10297f4d7f0f1734922377c8d11d17
Robert Willaim <robertdettenborn@gmail.com>
2026-08-05 20:55:52 -0300

    Arquivos calculadora

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:projeto/screens/myhomepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'aula01',
+      theme: ThemeData(
+
+        colorScheme: .fromSeed(seedColor: Colors.white),
+      ),
+      home: const MyHomePage(title: "Calculadora Digital 1.0"),
+    );
+  }
+}
+
---
+++ lib/screens/myhomepage.dart
@@ -0,0 +1,263 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double _counter1 = 0;
+  double _counter2 = 0;
+  double _result = 0;
+
+  void _incrementCounter1() {
+    setState(() {
+      _counter1++;
+    });
+  }
+  void _decrementCounter1() {
+    setState(() {
+      _counter1--;
+    });
+  }
+
+  void _incrementCounter2() {
+    setState(() {
+      _counter2++;
+    });
+  }
+  void _decrementCounter2() {
+    setState(() {
+      _counter2--;
+    });
+  }
+
+  void _somar() {
+    setState(() {
+      _result = _counter1 + _counter2;
+    });
+  }
+  void _subtrair() {
+    setState(() {
+      _result = _counter1 - _counter2;
+    });
+  }
+  void _multiplicar() {
+    setState(() {
+      _result = _counter1 * _counter2;
+    });
+      }
+  void _dividir() {
+  setState(() {
+  _result = _counter1 / _counter2;
+  });
+  }
+
+  void _limpar() {
+    setState(() {
+      _result = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: .center,
+          children: [
+            const Text('Primeiro Número'),
+            Text(
+              '$_counter1',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+
+            const SizedBox(height: 20),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                            fontSize: 20
+                        ),
+                        backgroundColor: Colors.lightBlueAccent
+                    ),
+                    onPressed: (){
+                      _incrementCounter1();
+                    },
+                    child: Text('Aumentar')
+                ),
+
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                            fontSize: 20
+                        ),
+                        backgroundColor: Colors.redAccent
+                    ),
+                    onPressed: (){
+                      _decrementCounter1();
+                    },
+                    child: Text('Diminuir')
+                )
+              ],
+            ),
+
+            const SizedBox(height: 40),
+
+            const Text('Segundo Número'),
+            Text(
+              '$_counter2',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+
+            const SizedBox(height: 20),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      textStyle: TextStyle(
+                        fontSize: 20
+                      ),
+                      backgroundColor: Colors.lightBlueAccent
+                  ),
+                    onPressed: (){
+                      _incrementCounter2();
+                    },
+                    child: Text('Aumentar')
+                ),
+
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                            fontSize: 20
+                        ),
+                        backgroundColor: Colors.redAccent
+                    ),
+                    onPressed: (){
+                      _decrementCounter2();
+                    },
+                    child: Text('Diminuir')
+                )
+              ],
+            ),
+
+            const SizedBox(height: 40),
+
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                            fontSize: 20
+                        ),
+                        backgroundColor: Colors.blue
+                    ),
+                    onPressed: (){
+                      _somar();
+                    },
+                    child: Text('+')
+                ),
+
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                            fontSize: 20
+                        ),
+                        backgroundColor: Colors.blue
+                    ),
+                    onPressed: (){
+                      _subtrair();
+                    },
+                    child: Text('-')
+                ),
+
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                            fontSize: 20
+                        ),
+                        backgroundColor: Colors.blue
+                    ),
+                    onPressed: (){
+                      _multiplicar();
+                    },
+                    child: Text('*')
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                            fontSize: 20
+                        ),
+                        backgroundColor: Colors.blue
+                    ),
+                    onPressed: (){
+                      _dividir();
+                    },
+                    child: Text('/')
+                ),
+              ],
+            ),
+            const SizedBox(height: 40),
+            const Text('Resultado'),
+            Text(
+              '$_result',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                            fontSize: 20
+                        ),
+                        backgroundColor: Colors.red
+                    ),
+                    onPressed: (){
+                      _limpar();
+                    },
+                    child: Text('Limpar')
+                ),
+
+              ],
+            ),
+
+          ],
+        ),
+
+
+
+      ),
+
+      
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter1,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+
+    );
+  }
+
+
+
+}
